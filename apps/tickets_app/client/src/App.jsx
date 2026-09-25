a6aa298aca5b4138d9681a59d087370e8153e2c0
keerthana Y T <2025cs_keerthanayt_c@nie.ac.in>
2026-09-25 15:18:38 +0530

    Create App.jsx

---
+++ apps/tickets_app/client/src/App.jsx
@@ -0,0 +1,20 @@
+import Login from "./Login";
+function Navigation() {
+    
+    return (
+       <>
+       
+       </>
+    );
+}
+
+function App() {
+    return (
+        <>
+        <Login/>
+       </>
+    );
+}
+
+export default App;
+
